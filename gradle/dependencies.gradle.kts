cdcf05e94b6535700b022d7351c91ec146629234
Victor Alenkov <victor@alenkov.name>
2018-09-05 10:01:46 +0300

    upgraded dependencies versions

--- gradle/dependencies.gradle.kts
+++ gradle/dependencies.gradle.kts
@@ -19,22 +19,22 @@ configurations.all {
         "junit" -> useVersion("4.12")
         "org.junit.platform" -> useVersion("1.2.0")
         "org.junit.jupiter" -> useVersion("5.2.0")
-        "org.mockito" -> useVersion("2.18.3")
+        "org.mockito" -> useVersion("2.21.0")
         "org.opentest4j" -> useVersion("1.1.0")
         "org.objenesis" -> useVersion("2.6")
         "org.jetbrains.kotlin" -> useVersion(kotlinVersion)
-        "io.kotlintest" -> useVersion("3.1.6")
-        "io.github.glytching" -> useVersion("2.2.0")
+        "io.kotlintest" -> useVersion("3.1.9")
+        "io.github.glytching" -> useVersion("2.3.0")
         "org.apache.commons" -> when (requested.name) {
           "commons-lang3" -> useVersion("3.4")
           "commons-compress" -> useVersion("1.17")
         }
         "org.apache.httpcomponents" -> when (requested.name) {
-          "httpclient" -> useVersion("4.5.2")
-          "httpcore" -> useVersion("4.4.5")
+          "httpclient" -> useVersion("4.5.6")
+          "httpcore" -> useVersion("4.4.10")
         }
-        "com.google.code.gson" -> useVersion("2.8.2")
-        "io.github.microutils" -> useVersion("1.5.4")
+        "com.google.code.gson" -> useVersion("2.8.5")
+        "io.github.microutils" -> useVersion("1.6.10")
         "com.github.itbasis.kotlin-utils" -> useVersion("v20180618_1139")
       }
     }
